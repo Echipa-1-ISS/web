2b9bb2dba2ef32cfe34378fbea98d425b61aeef9
Teodor Filp <tudorf90@hotmail.com>
2022-03-28 18:29:07 +0300

    added dockerfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// jsx
 function App() {
   return (
     <div className="App">
