2f3a4f6a56cea05fa5b9cc74e739df4689855fe1
DianaD <diana_draghiciu@yahoo.com>
2022-04-19 10:04:59 +0300

    userProfile request

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,18 +1,23 @@
-import { AxiosResponse } from "axios";
+import {AxiosResponse} from "axios";
 import api from "../api";
-import { ApiEndpoints } from "../api/endpoints";
+import {ApiEndpoints} from "../api/endpoints";
 
 class UserService {
-  public login(username: string, password: string) {
-    return api
-      .post(ApiEndpoints.login, { username, password })
-      .then(({ data: { userId, token } }: AxiosResponse<any>) => {
-        localStorage.setItem("access_token", token);
-        localStorage.setItem("userId", userId);
+    public login(username: string, password: string) {
+        return api
+            .post(ApiEndpoints.login, {username, password})
+            .then(({data: {userId, token}}: AxiosResponse<any>) => {
+                localStorage.setItem("access_token", token);
+                localStorage.setItem("userId", userId);
 
-        return true;
-      });
-  }
+                return true;
+            });
+    }
+
+    public getProfileInfo(userId: number) {
+        return api
+            .get(ApiEndpoints.userProfile, {params: {userId: userId}})
+    }
 }
 
 export default new UserService();
